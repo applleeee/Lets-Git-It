08bae22da4e8a9ce8a5c7b03aac440d03f586730
MatheGoD <mathe1303@gmail.com>
2023-02-10 11:39:31 +0900

    쩝

--- src/rank/rank.controller.ts
+++ src/rank/rank.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { RankService } from './rank.service';
 
 @Controller('ranks')
