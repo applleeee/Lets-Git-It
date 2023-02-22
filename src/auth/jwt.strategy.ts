a60278d6c6e8f66a4bce8be58de7545fb3dcdc88
myeongseoklee <yhkyhk92@gmail.com>
2023-02-22 14:22:04 +0900

    토근 유효기간 무시 설정

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -14,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
+      ignoreExpiration: true,
       secretOrKey: jwtConstants.secret,
     });
   }
